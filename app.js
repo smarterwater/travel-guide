e9ce42dc7a06441b2228ec3eb054e50c129ff301
Bumjargal Boldbaatar <bumaaka1@gmail.com>
2020-04-09 23:33:11 -0500

    heroku changes

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const accountsRouter = require('./controllers/accounts')
+const path = require('path');           
 const mongoose = require('mongoose')
 require('dotenv').config()
 const MONGODB_URI = process.env.MONGODB_URI
+
+app.use(cors())
+app.use(express.json())
+app.use('/', accountsRouter)
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
@@ -14,10 +19,14 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true 
         console.log('Error: ' + error)
     })
 
-// app.use(express.static('build'))
-app.use(cors())
-app.use(express.json())
-app.use('/', accountsRouter)
+if (process.env.NODE_ENV === 'production') {           
+    app.use(express.static('client/build'));
+    
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 
 module.exports = app
 
