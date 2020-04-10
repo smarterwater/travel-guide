c302455b710857cbff3205199f7b963cc8d7ed75
Bumjargal Boldbaatar <bumaaka1@gmail.com>
2020-04-10 15:00:51 -0500

    more stuff

--- client/src/components/CenterForm.js
+++ client/src/components/CenterForm.js
@@ -10,25 +10,41 @@ const CenterForm = (props) => {
         password: ''
     })
 
+    const [whichBtn, setWhichBtn] = useState ('')
+
     const handleSubmit = (event) => {
         event.preventDefault()
-        console.log('email: ' + user.email + ' pass: ' + user.password)
+        console.log('email: ' + user.email + ' password: ' + user.password)
         if(!user.email || !user.password) return
-        const register = async () => {
-            try {
-                const response = axios.post('/api/accounts', user)
-                console.log(response)
-                props.history.push('/home')
-            } catch(error) {
-                console.log('error', error)
+        if (whichBtn === 'signup') {
+            const register = async () => {
+                try {
+                    const response = axios.post('/api/accounts', user)
+                    console.log(response)
+                    props.history.push('/home')
+                } catch(error) {
+                    console.log('error', error)
+                }
             }
+            register();
         }
-        register();
+        else if (whichBtn === 'login') {
+            const login = async () => {
+                try {
+                    const response = axios.get('/api/accounts/find', user)
+                    console.log(response)
+                    //props.history.push('/home')
+                } catch(error) {
+                    console.log('error', error)
+                }
+            }
+            login();
+        }
     }
 
     return (
         <div className="CenterForm">
-            <div style={h2Style} className='Title'>
+            <div style={{ position: 'absolute', top: '50px'}} className='Title'>
                 <Typing speed={100}>
                     <h2>Alexa, Be My Travel Guide</h2>
                 </Typing>
@@ -43,11 +59,11 @@ const CenterForm = (props) => {
                     <Form.Control type="password" value={user.password} placeholder="Password" onChange={event => setUser({ ...user, password: event.target.value})}/>
                 </Form.Group>
 
-                <Button variant="outline-secondary" type="submit" style={{width: 150}}>
+                <Button variant="outline-secondary" type="submit" style={{width: 150}} onClick={event => setWhichBtn('login')}>
                     Login
                 </Button>
                 <br/>
-                <Button variant="outline-secondary" type="submit" style={{width: 150}}>
+                <Button variant="outline-secondary" type="submit" style={{width: 150}} onClick={event => setWhichBtn('signup')}>
                      Sign Up
                 </Button>
             </Form>
